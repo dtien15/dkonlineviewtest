cead4bc78d19e45e3da49080fb66a1ce50f55865
dtien15 <dangtien.tdt@gmail.com>
2025-05-14 16:21:52 +0700

    first commist

--- js/vnkeyboard.js
+++ js/vnkeyboard.js
@@ -85,7 +85,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // --- Idle-modal + timeout ---
   let idleTimer = null,
     isModalOpen = false;
-  const idleTimeout = 9000,
+  const idleTimeout = 9000000,
     page1 = document.getElementById("page1");
   const page2 = document.getElementById("page2");
   const idleModal = document.getElementById("idleModal");
